7521a0146398e12af037e2f10994602fbb18448e
n96js <n96js@naver.com>
2024-01-04 15:51:17 +0900

    js

---
+++ UI_005/js/nav.js
@@ -0,0 +1,3 @@
+const_nav_home = document.querySelector("li.nav_main.home");
+const_nav_login = document.querySelector("li.nav_main.login");
+const_nav_join = document.querySelector("li.nav_main.join");
